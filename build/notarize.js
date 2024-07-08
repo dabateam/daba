e6c0ddfec942c76f96cf2fc4e92cc8ddea950033
Zakaria Najem <nzikero@gmail.com>
2024-07-08 18:35:37 +0700

    debugging apple notarizing + github actions

--- build/notarize.js
+++ build/notarize.js
@@ -1,21 +1,23 @@
-const { notarize } = require('@electron/notarize')
+const { notarize } = require("@electron/notarize")
 
 module.exports = async (context) => {
-  if (process.platform !== 'darwin') return
+  if (process.platform !== "darwin") return
 
-  console.log('aftersign hook triggered, start to notarize app.')
+  console.log("aftersign hook triggered, start to notarize app.")
 
   if (!process.env.CI) {
     console.log(`skipping notarizing, not in CI.`)
     return
   }
 
-  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
-    console.warn('skipping notarizing, APPLE_ID and APPLE_ID_PASS env variables must be set.')
+  if (!("APPLE_ID" in process.env && "APPLE_ID_PASS" in process.env)) {
+    console.warn(
+      "skipping notarizing, APPLE_ID and APPLE_ID_PASS env variables must be set.",
+    )
     return
   }
 
-  const appId = 'com.electron.app'
+  const appId = "so.daba.app"
 
   const { appOutDir } = context
 
@@ -26,7 +28,8 @@ module.exports = async (context) => {
       appBundleId: appId,
       appPath: `${appOutDir}/${appName}.app`,
       appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLEIDPASS
+      appleIdPassword: process.env.APPLE_ID_PASS,
+      teamId: process.env.APPLE_TEAM_ID,
     })
   } catch (error) {
     console.error(error)
