ee79c8e4646e81597905fdf35c04300cf44655be
Zakaria Najem <nzikero@gmail.com>
2024-08-08 18:07:55 +0700

    scrollable apps side menu

--- src/renderer/src/lib/components/NewProject/NewProjectApps.svelte
+++ src/renderer/src/lib/components/NewProject/NewProjectApps.svelte
@@ -8,7 +8,7 @@
 {#if store.flow === "DIY"}
   <div
     style:width={NEW_PROJECT_APPS_WIDTH + "px"}
-    class="border-r border-white/5 absolute top-0 h-full left-0 overflow-auto flex flex-col items-center gap-[20px] pt-[22px]"
+    class="border-r pb-[20px] border-white/5 absolute top-0 h-full left-0 overflow-auto flex flex-col items-center gap-[20px] pt-[22px]"
   >
     <div class="text-white/30 mb-[62px] text-[11px]">New project</div>
     <div class="text-[11px] mb-[16px]">Apps</div>
@@ -21,7 +21,7 @@
           store.showAddApp = false
         }}
         class={cn(
-          "active:bg-white/[0.02] hover:bg-white/[0.01] size-[80px] rounded-[8px] border border-white/10 flex items-center justify-center gap-[12px] flex-col",
+          "shrink-0 active:bg-white/[0.02] hover:bg-white/[0.01] size-[80px] rounded-[8px] border border-white/10 flex items-center justify-center gap-[12px] flex-col",
 
           store.currentApp === app.name &&
             "ring-[2px] ring-[#40AFFF]/80 border-transparent bg-white/[0.02] hover:bg-white/[0.02]",
@@ -41,7 +41,7 @@
         store.currentApp = ""
       }}
       class={cn(
-        "active:bg-white/[0.02] hover:bg-white/[0.01] size-[80px] rounded-[8px] flex items-center justify-center gap-[12px] flex-col border-dashed",
+        "shrink-0 active:bg-white/[0.02] hover:bg-white/[0.01] size-[80px] rounded-[8px] flex items-center justify-center gap-[12px] flex-col border-dashed",
         store.showAddApp &&
           "ring-[2px] ring-[#40AFFF]/80 border-transparent bg-white/[0.02] hover:bg-white/[0.02]",
       )}
